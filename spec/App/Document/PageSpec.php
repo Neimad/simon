e568b2da29f768e496536a43c230804b62a0d24e
Damien Flament <damien.flament@gmx.com>
2018-01-21 02:05:28 +0100

    Installed phpspec

---
+++ spec/App/Document/PageSpec.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace spec\App\Document;
+
+use App\Document\Page;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class PageSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(Page::class);
+    }
+
+    function it_has_a_title()
+    {
+        $title = 'foo';
+
+        $this->setTitle($title);
+        $this->getTitle()->shouldReturn($title);
+    }
+
+    function it_has_a_content()
+    {
+        $content = 'Foo bar baz.';
+
+        $this->setContent($content);
+        $this->getContent()->shouldReturn($content);
+    }
+}
